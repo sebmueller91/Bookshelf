5281aaa484ec1c95aee43177f1d88f859de7294c
Sebastian Müller <seb.mueller.91@web.de>
2022-11-25 16:34:50 +0100

    implemented search bar

--- Bookshelf/app/src/main/java/com/example/bookshelf/ui/screens/QueryScreen.kt
+++ Bookshelf/app/src/main/java/com/example/bookshelf/ui/screens/QueryScreen.kt
@@ -1,12 +1,78 @@
 package com.example.bookshelf.ui.screens
 
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.bookshelf.R
+
 
 @Composable
 fun QueryScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "wow wow wow"
-    )
+    var text by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+    val lightBlue = Color(0xffd8e6ff)
+    val blue = Color(0xff76a9ff) // TODO: Move out
+
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        horizontalAlignment = CenterHorizontally
+    ) {
+        Text(
+            stringResource(R.string.SearchBookLibrary),
+            modifier = modifier.align(CenterHorizontally),
+            fontSize = 20.sp,
+            style = MaterialTheme.typography.h6
+        )
+        TextField(
+            value = text,
+            onValueChange = { x: String ->
+                text = x
+            },
+            singleLine = true,
+            shape = RoundedCornerShape(16.dp),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                capitalization = KeyboardCapitalization.Words,
+                autoCorrect = false,
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = { focusManager.clearFocus() }),
+            leadingIcon = {
+                Icon(
+                    Icons.Default.Search,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .padding(15.dp)
+                        .size(24.dp)
+                )
+            },
+            colors = TextFieldDefaults.textFieldColors(
+                backgroundColor = lightBlue,
+                cursorColor = androidx.compose.ui.graphics.Color.Black,
+                focusedIndicatorColor = androidx.compose.ui.graphics.Color.Transparent,
+                unfocusedIndicatorColor = androidx.compose.ui.graphics.Color.Transparent
+            ),
+        )
+    }
 }
